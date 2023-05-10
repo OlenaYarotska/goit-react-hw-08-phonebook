a5f242bea05d8d3eb48da6552f8a5b33ef13030b
OlenaYarotska <olena.zhurba@gmail.com>
2023-05-10 18:40:35 +0300

    add style fixes

--- src/Components/Container/Container.styled.jsx
+++ src/Components/Container/Container.styled.jsx
@@ -4,6 +4,7 @@ import background_picture from '../../Images/v748-toon-106-min.jpg';
 export const AppWrapper = styled.div`
 margin: 0 auto;
 max-width: 900px;
+height: 100 vh;
 background-image: url(${background_picture}) ;
 background-size: cover;
 background-repeat: repeat;
